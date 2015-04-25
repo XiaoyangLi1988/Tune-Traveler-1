63fdf581fbfa05de448d936f95851373d7c7b92c
Darin Beaudreau <darinbeaudreau@gmail.com>
2015-04-25 19:58:10 -0400

    Changed player movement to reflect PATH.
    Corrected PATH retracing function so it now generates the path from
    start to goal properly and changed the player movement to follow the
    path. (Holy crap, it works!)

--- constants.rkt
+++ constants.rkt
@@ -15,6 +15,7 @@
 (define GRID_OFF (/ (abs (- F_WIDTH F_HEIGHT)) 2))
 (define A_TIMER 0)
 (define PATH '())
+(define PATH-POS 0)
 
 ; Setup the sound settings and load the guitar sounds.
 (define-runtime-path demos "demos")
@@ -111,7 +112,8 @@
         (begin (set! current (send current getParent))
                (set! PATH (append PATH (list current)))
                (retrace-help))))
-    (retrace-help)))
+    (retrace-help)
+    (set! PATH (reverse PATH))))
 
 ; Look through parents to find start.
 ;(define (parent-ception C n)
@@ -134,48 +136,41 @@
              (set! closed (append closed (list current)))          ; Add the current tile to the closed list since we're done "exploring" it.
              (set! open (remove current open))                     ; Remove the current tile from the open list so we don't accidentally "explore" it again.
              (unless (sameTile? current B)                         ; Unless this is the goal tile, keep searching. Otherwise, we're done here.
-              (begin (set! neighbors (getNeighbors GRID current))  ; Retrieve the 8 neighbor tiles surrounding the current tile. Un-walkable tiles excluded.
-                     (map (lambda (t)                              ; Map over each neighbor tile...
-                            (unless (member t closed)              ; Ignore tiles that are on the closed list... we've already "explored" them.
-;                              (if (not (member t open))            ; If the tile is not yet on the open list, add it to the open list and compute its F score.
-;                                  (begin (send t setG (compG current t))                   ; Compute the G score of the neighbor.
-;                                         (send t setH (compH t B))                         ; Compute the H score of the neighbor.
-;                                         (send t setF (compF (send t getG) (send t getH))) ; Compute the F score of the neighbor.
-;                                         (send t setParent current)                        ; Set the parent of the neighbor to the current tile.
-;                                         (set! open (append open (list t))))               ; Add the neighbor to the open list.
-;                                  ; If it's on the open list, but not the closed list, see if it's a better path than the current tile.
-;                                  ())
-                              (when (not (member t open))
-                                (begin (send t setG (compG current t))                    ; Compute the G score of the neighbor.
-                                         (send t setH (compH t B))                         ; Compute the H score of the neighbor.
-                                         (send t setF (compF (send t getG) (send t getH))) ; Compute the F score of the neighbor.
-                                         (send t setParent current)                        ; Set the parent of the neighbor to the current tile.
-                                         (set! open (append open (list t)))))))
-                          neighbors)
-                     (unless (empty? open) (searchLoop))))))       ; If there are no more tiles in the open list, we're done searching.
+               (begin (set! neighbors (getNeighbors GRID current))  ; Retrieve the 8 neighbor tiles surrounding the current tile. Un-walkable tiles excluded.
+                      (map (lambda (t)                              ; Map over each neighbor tile...
+                             (unless (member t closed)              ; Ignore tiles that are on the closed list... we've already "explored" them.
+                               ;                              (if (not (member t open))            ; If the tile is not yet on the open list, add it to the open list and compute its F score.
+                               ;                                  (begin (send t setG (compG current t))                   ; Compute the G score of the neighbor.
+                               ;                                         (send t setH (compH t B))                         ; Compute the H score of the neighbor.
+                               ;                                         (send t setF (compF (send t getG) (send t getH))) ; Compute the F score of the neighbor.
+                               ;                                         (send t setParent current)                        ; Set the parent of the neighbor to the current tile.
+                               ;                                         (set! open (append open (list t))))               ; Add the neighbor to the open list.
+                               ;                                  ; If it's on the open list, but not the closed list, see if it's a better path than the current tile.
+                               ;                                  ())
+                               (when (not (member t open))
+                                 (begin (send t setG (compG current t))                    ; Compute the G score of the neighbor.
+                                        (send t setH (compH t B))                         ; Compute the H score of the neighbor.
+                                        (send t setF (compF (send t getG) (send t getH))) ; Compute the F score of the neighbor.
+                                        (send t setParent current)                        ; Set the parent of the neighbor to the current tile.
+                                        (set! open (append open (list t)))))))
+                           neighbors)
+                      (unless (empty? open) (searchLoop))))))       ; If there are no more tiles in the open list, we're done searching.
     (searchLoop)
-    (display (sameTile? ((get (cdr start) (car start)) GRID) ((get (cdr start) (car start)) GRID)))
-    (send B setParent current)
-    (retrace GRID A B)))
+    (retrace GRID A current)))
 
 ; Used to move the player around the grid.
 (define (move rOff cOff)
-  (set! player (cons (+ (car player) rOff) (+ (cdr player) cOff)))   ; Modifies the player's position to reflect the specified movement.
+  (set! player (cons (+ (car player) cOff) (+ (cdr player) rOff)))   ; Modifies the player's position to reflect the specified movement.
   (play (list-ref clips (random 4))))                                ; Plays one of the guitar sound clips.
 
 ; Called after the window renders. Used to update objects.
 (define (update)
-  (if (>= A_TIMER 1/3)                          ; We want the player to move every 0.5 seconds, so the timer stops at 1/2 second and then acts.
+  (if (>= A_TIMER 1/2)                          ; We want the player to move every 0.5 seconds, so the timer stops at 1/2 second and then acts.
       (begin (set! A_TIMER 0)                   ; Reset the timer, then check to see where the player is and move accordingly.
-             (cond ((< (car player) (car goal)) ; If the player is above the goal, move them one down.
-                    (move 1 0))  ; Move down.
-                   ((> (car player) (car goal)) ; If the player is below the goal, move them one up.
-                    (move -1 0)) ; Move up.
-                   ((< (cdr player) (cdr goal)) ; If the player is to the left of the goal, move them one to the right.
-                    (move 0 1))  ; Move right.
-                   ((> (cdr player) (cdr goal)) ; If the player is to the right of the goal, move them one to the left.
-                    (move 0 -1)) ; Move left.
-                   (else #f)))
+             (when (< PATH-POS (- (length PATH) 0))
+               (move (- (send (list-ref PATH PATH-POS) getRow) (cdr player))
+                     (- (send (list-ref PATH PATH-POS) getCol) (car player)))
+               (set! PATH-POS (+ PATH-POS 1))))
       (set! A_TIMER (+ A_TIMER 1/60))))         ; If a half a second hasn't gone by yet, add the time delay to the timer.
 
 (provide (all-defined-out))
